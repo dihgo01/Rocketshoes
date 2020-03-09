3b06116414823faeef941a4f8e86d81216d67ce7
dihgo01 <luka.poke9@gmail.com>
2020-03-09 19:47:17 -0300

    Styled the cart and home

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md';
+import doninha from '../../assets/img/doninha.jpg'
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={doninha} alt="Tenis" />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$ 127,78</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type= "number" readOnly value={2}/>
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1297,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 import Tenis from '../../assets/img/doninha.jpg'
@@ -8,6 +9,51 @@ export default function Home() {
     <ProductList>
       <li>
         <img src={Tenis} alt="Tenis" />
+        <strong>Doninha da hora</strong>
+        <span>R$ 120,00</span>
+
+        <button type="button">
+           <div>
+              <MdAddShoppingCart size={16} color="#fff" />3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={Tenis} alt="Tenis" />
+        <strong>Doninha da hora</strong>
+        <span>R$ 120,00</span>
+
+        <button type="button">
+           <div>
+              <MdAddShoppingCart size={16} color="#fff" />3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={Tenis} alt="Tenis" />
+        <strong>Doninha da hora</strong>
+        <span>R$ 120,00</span>
+
+        <button type="button">
+           <div>
+              <MdAddShoppingCart size={16} color="#fff" />3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={Tenis} alt="Tenis" />
+        <strong>Doninha da hora</strong>
+        <span>R$ 120,00</span>
+
+        <button type="button">
+           <div>
+              <MdAddShoppingCart size={16} color="#fff" />3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+        </button>
       </li>
     </ProductList>
   );
